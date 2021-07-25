aaee1d665cb78ed6a66f0e4d72e02f3b8cb967c8
Navdeep K <kantenavdeep@outlook.com>
2021-07-25 20:27:54 +0530

    something

--- script.js
+++ script.js
@@ -3,7 +3,7 @@
 // Code injected for typed.js
 
 var typed = new Typed('#animate', {
-    strings: ["Front-End Web Developer", "Programmer", "Pythonista", "JavaScript Lover", "Sketch Artist", ],
+    strings: ["Programmer", "Pythonista", "JavaScript Lover", "Sketch Artist", ],
     typeSpeed: 50,
     backSpeed: 30,
     loop: true
